9650fbab65ebe52ab13b780c8acde3d0b7fdcfba
Gregorio John Marc A <122092610+1Cealus@users.noreply.github.com>
2023-11-06 17:56:46 +0800

    Made templates/starting point for cache py and simulator py

--- main.py
+++ main.py
@@ -1,38 +1,36 @@
-class CacheBlock:
-    def __init__(self):
-        self.tag = None
-        self.valid = False
-        self.lru_counter = 0
-        # Include other metadata as required.
+from cache import Cache
 
-class CacheSet:
-    def __init__(self, associativity):
-        self.blocks = [CacheBlock() for _ in range(associativity)]
+def test_cache_miss(cache, address):
+     initial_miss_count = cache.miss_count
+     cache.access_block(address)
+     assert cache.miss_count == initial_miss_count + 1, "Cache miss count should increment by 1"
+     print("Test Cache Miss: PASSED")
 
-class Cache:
-    def __init__(self, num_sets, associativity, block_size):
-        self.sets = [CacheSet(associativity) for _ in range(num_sets)]
-        self.block_size = block_size
-        # Initialize other attributes like stats counters.
+def test_cache_hit(cache, address):
+     cache.access_block(address)  # Initial access - should be miss and load the block
+     initial_hit_count = cache.hit_count
+     cache.access_block(address)  # Second access - should be hit
+     assert cache.hit_count == initial_hit_count + 1, "Cache hit count should increment by 1"
+     print("Test Cache Hit: PASSED")
 
-    def access_block(self, address):
-        # Determine the set for this address.
-        # Check for hit/miss and update LRU counters.
-        # Update the block if it's a miss.
-        pass
-
-    def get_stats(self):
-        # Return statistics like hit count, miss count, etc.
-        pass
-
-    # Include other methods for cache operations and statistics.
 
 def simulate_cache():
     cache = Cache(32, 8, 16)
-    # Perform your test cases a.), b.), and c.) here.
-    # Update cache on each memory access and track statistics.
-    # Print or write to a file the final statistics and cache snapshot.
+    # Implement your test cases here and interact with the Cache instance.
+    # After the simulation, get and print out/write to file the stats.
+    hit_rate, miss_rate, hit_count, miss_count = cache.get_stats()
+    # Print the stats or write to a file as required.
+    # ...
 
 # Main function to kick off the simulation.
 if __name__ == "__main__":
-    simulate_cache()
+    # Initialize cache with 32 sets, 8-way set associative, and block size of 16 words.
+    cache = Cache(32, 8, 16)
+    
+    # Run simple tests
+    test_cache_miss(cache, '0x0000')  # Test with an address that should miss.
+    test_cache_hit(cache, '0x0000')   # Test with the same address, which should now hit.
+    # ... more tests ...
+    
+    # Output the result of the tests
+    print("All tests completed.")
